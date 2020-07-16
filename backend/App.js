a9738ca298281d33b4488b9cf3204b551ee49cc3
anand iyengar <anandiyengar0@gmail.com>
2020-07-16 14:51:38 +0530

    PORT

--- backend/App.js
+++ backend/App.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 4040;
 const mongoose = require("mongoose");
 
 // IMPORT ALL THE REQUIRED LIBRARIES
